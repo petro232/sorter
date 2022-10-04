ca3de665489b61eb0c711570adf5e819ff8f514d
Andrés <andpetronio@outlook.com>
2022-10-04 18:18:38 -0300

    server branch

--- sorter.js
+++ sorter.js
@@ -1,64 +1,17 @@
-import {
-  mainMenu,
-  pause,
-  userInput,
-  pathsListToDelete,
-  confirm,
-  sortMethod
-} from './helpers/inquirer.js';
+import express from 'express';
+const app = express();
+const port = 3000;
 
-import Paths from './models/paths.js';
-import Core from './helpers/core.js'
+app.use('/', express.static('public'));
+app.get('/', (req, res) => { })
+// app.post('/', (req, res) => { console.log('post to index') })
 
-const main = async () => {
-  let opt = '';
-  const paths = new Paths();
-  const core = new Core();
 
-  do {
-    opt = await mainMenu();
-    switch (opt) {
-      case '1':
-        // clasificar archivos
-        const method = await sortMethod();
-        await core.sort(method);
-        console.log('Distribución de documento finalizada'.green)
-        break;
-      case '2':
-        // ver paths
-        paths.getAllPaths();
-        break;
-      case '3':
-        // config path de origen
-        const sourcePath = await userInput(`Ingrese el ${'Path:'.cyan}`);
-        const okAddingSourcePath = await confirm('Está segura/o de agregar o cambiar el path de origen?');
-        if (okAddingSourcePath) {
-          paths.addPath({ dir: sourcePath, type: 'source' });
-          console.log('Path agregado correctamente'.green)
-        }
-        break;
-      case '4':
-        // config paths de destino
-        const destinationPath = await userInput(`Ingrese el ${'Path:'.cyan}`);
-        const okAddingDestinationPath = await confirm('Está segura/o de agregar el path de destino?');
-        if (okAddingDestinationPath) {
-          paths.addPath({ dir: destinationPath, type: 'destination' });
-          console.log('Path agregado correctamente'.green)
-        }
-        break;
-      case '5':
-        const pathToDeleteID = await pathsListToDelete();
-        const ok = await confirm('Está segura/o de borrar el path seleccionado?');
-        if (ok) {
-          paths.deletePath(pathToDeleteID);
-          console.log('Path borrado correctamente'.green)
-        }
-        break;
-    }
 
-    await pause();
 
-  } while (opt !== '6');
 
-}
-main();
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
